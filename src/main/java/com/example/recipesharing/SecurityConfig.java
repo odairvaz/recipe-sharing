77ad1d5d7439f5e25f8e581c415f1ccee658c009
Odair Vaz <vazodair@gmail.com>
2025-04-11 17:14:55 +0200

    Remove un-used variable.

--- src/main/java/com/example/recipesharing/SecurityConfig.java
+++ src/main/java/com/example/recipesharing/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.example.recipesharing;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,8 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig  {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(SecurityConfig.class);
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
