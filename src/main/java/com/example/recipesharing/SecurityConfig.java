3f4ccac58a07555e31cbc278b1f7d2d80057280f
Odair Vaz <vazodair@gmail.com>
2025-04-11 17:10:13 +0200

    Fix after review.git push

--- src/main/java/com/example/recipesharing/SecurityConfig.java
+++ src/main/java/com/example/recipesharing/SecurityConfig.java
@@ -20,7 +20,7 @@ public class SecurityConfig  {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/static/css/**", "/js/**").permitAll()
+                        .requestMatchers("/login", "/css/**", "/static/css/**", "/js/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(login -> login
